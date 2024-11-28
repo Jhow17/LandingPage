908f94c234ea3d86fe2bcd967fbe8e9b73f3b817
Jhow17 <silvajonathan234@gmail.com>
2024-11-28 11:57:00 -0300

    Muda o estilo da seções adiciona cria as funçoes de validaçao e de envio das informaçoes

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
 // Função para exibir um alerta de contato
-function showContactAlert() {
-    alert('Obrigado por entrar em contato! Nossa equipe retornará em breve.');
- }
+const loginForm =   document.querySelector('#loginForm')
+const email =document.querySelector('#email')
+const senha = document.querySelector('#senha')
+const contatoForm = document.querySelector('#form-contato')
+const nome = document.querySelector('#nome')
+const assunto = document.querySelector('#assunto')
+const telefone = document.querySelector('#telefone')
+const mensagem = document.querySelector('#mensagem')
+
+loginForm.addEventListener("submit",validateLogin)
+contatoForm.addEventListener("submit", enviaContato)
  // Função para validar o login
  function validateLogin(event) {
     event.preventDefault(); // Impede o envio do formulário
-    const email = loginForm.email.value;
-    const senha = loginForm.senha.value;
     // Verifica se o login é válido (exemplo de como deve ser )
-    if (email === 'admin@example.com' && senha === 'password') {
+    if (email.value === 'admin@example.com' && senha.value === 'password') {
         alert('Login realizado com sucesso!');
     } else {
-        alert('Erro ao realizar login. Tente novamente.');
+        alert('E-mail ou senha incorretos.');
     }
  }
- // Função para exibir um alerta ao clicar no botão "Saiba Mais"
- function showMoreInfoAlert() {
-    alert('Você está prestes a descobrir como o sistema de notificação pode ajudar o seu hospital!');
- }
- // Função para adicionar os eventos aos elementos
- function addEventListeners() {
-    const contatoButton = document.querySelector('.btn'); // Botão de contato
-    const loginForm = document.querySelector('#login'); // Formulário de login
-    const saibaMaisButton = document.querySelector('.btn'); // Botão "Saiba Mais"
-    // Adiciona evento de clique ao botão de contato
-    contatoButton.addEventListener('click', showContactAlert);
-    // Adiciona evento de submit ao formulário de login
-    loginForm.addEventListener('submit', validateLogin);
-    // Adiciona evento de clique ao botão "Saiba Mais"
-    saibaMaisButton.addEventListener('click', showMoreInfoAlert);
- }
- // Função principal para inicializar o script
- function init() {
-    addEventListeners(); // Chama a função para adicionar os eventos
- }
- // Inicializa o script quando o DOM estiver completamente carregado
- document.addEventListener('DOMContentLoaded', init);
+ function enviaContato(event) {
+   event.preventDefault(); 
+   alert('Obrigado por entrar em contato ! Suas informações ja foram enviadas')
+   alert(`Nome: ${nome.value}\n Assunto: ${assunto.value}\n Telefone: ${telefone.value}\n Mensagem: ${mensagem.value}`)
+}
+
